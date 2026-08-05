4a94617aab8966de437971bd7863d8a2a0e67c2f
Dao Anh Tuan <24104005@st.phenikaa-uni.edu.vn>
2026-08-06 01:14:24 +0700

    cap nhat SinhVienThuong SinhVienUuTien

--- backend/src/model/SinhVienThuong.java
+++ backend/src/model/SinhVienThuong.java
@@ -1,4 +1,23 @@
 package model;
 
-public class SinhVienThuong {
+public class SinhVienThuong extends BanDoc {
+    public SinhVienThuong() {
+        super();
+        setLoaiBanDoc("Sinh Viên Thường");
+        setGioiHanMuon(3);
+
+    }
+    public SinhVienThuong(String maNguoiDung, 
+        String hoTen, 
+        String soDienThoai) {
+        
+            super(maNguoiDung, 
+                hoTen, 
+                soDienThoai,
+                "Sinh Viên Thường",
+                soSachDangMuon);
+
+        setGioiHanMuon(3);
+        
+    }
 }
