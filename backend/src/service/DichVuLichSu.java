eaea5ec9d18f2c1b68fe2db94322588f2262826e
PhiThuHuong <24100069@st.phenikaa-uni.edu.vn>
2026-08-05 21:33:39 +0700

    phi-thi-thu-huong

--- backend/src/service/DichVuLichSu.java
+++ backend/src/service/DichVuLichSu.java
@@ -1,4 +1,69 @@
 package service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DichVuLichSu {
+    private final repository.KhoPhieuMuon khoPhieuMuon;
+    private final repository.KhoPhieuTra khoPhieuTra;
+    private final repository.KhoBanDoc khoBanDoc;
+    private final repository.KhoSach khoSach;
+
+    public DichVuLichSu() {
+        khoPhieuMuon = new repository.KhoPhieuMuon();
+        khoPhieuTra = new repository.KhoPhieuTra();
+        khoBanDoc = new repository.KhoBanDoc();
+        khoSach = new repository.KhoSach();
+    }
+    public List<model.LichSu> layLichSu() {
+        List<model.LichSu> ketQua = new ArrayList<>();
+        for (model.PhieuMuon pm : khoPhieuMuon.getDanhSachPhieuMuon()) {
+            String tenBanDoc = pm.getMaBanDoc();
+            String tenSach = pm.getMaSach();
+            String ngayTra = "";
+
+            // tìm tên bạn đọc
+            for (model.BanDoc bd : khoBanDoc.getDanhSachBanDoc()) {
+
+                if (bd.getMaNguoiDung()
+                        .equals(pm.getMaBanDoc())) {
+                    tenBanDoc = bd.getHoTen();
+                    break;
+                }
+
+            }
+
+            // tìm tên sách
+            for (model.Sach sach : khoSach.getDanhSachSach()) {
+                if (sach.getMaSach()
+                        .equals(pm.getMaSach())) {
+                    tenSach = sach.getTenSach();
+                    break;
+                }
+
+            }
+
+            // tìm ngày trả
+            for (model.PhieuTra pt : khoPhieuTra.getDanhSachPhieuTra()) {
+                if (pt.getMaPhieuMuon()
+                        .equals(pm.getMaPhieuMuon())) {
+                    ngayTra = pt.getNgayTra();
+                    break;
+                }
+            }
+
+            model.LichSu lichSu = new model.LichSu(
+                    pm.getMaPhieuMuon(),
+                    tenBanDoc,
+                    tenSach,
+                    pm.getNgayMuon(),
+                    ngayTra,
+                    pm.getTrangThai()
+            );
+
+            ketQua.add(lichSu);
+        }
+
+        return ketQua;
+    }
 }
