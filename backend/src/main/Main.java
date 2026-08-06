907bdc58ebbcb9fcd32c500784b5d5b2dfa7f604
Tên của bạn <hongtdtk6@gmail.com>
2026-08-06 07:14:27 +0700

    Hoan thien main

--- backend/src/main/Main.java
+++ backend/src/main/Main.java
@@ -1,4 +1,62 @@
 package main;
 
+import com.sun.net.httpserver.HttpServer;
+import handler.*;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        HttpServer server = HttpServer.create(
+                new InetSocketAddress(8080),
+                0
+        );
+
+        // API sách
+        server.createContext(
+                "/api/sach",
+                new SachHandler()
+        );
+
+        // API bạn đọc
+        server.createContext(
+                "/api/bandoc",
+                new BanDocHandler()
+        );
+
+        // API mượn sách
+        server.createContext(
+                "/api/muon",
+                new MuonSachHandler()
+        );
+
+        // API trả sách
+        server.createContext(
+                "/api/tra",
+                new TraSachHandler()
+        );
+        //API lichsu
+        server.createContext(
+                "/api/lichsu",
+                new LichSuHandler()
+        );
+
+        server.setExecutor(null);
+
+        server.start();
+
+        System.out.println("SERVER ĐÃ KHỞI ĐỘNG");
+        System.out.println("Port: 8080");
+
+        System.out.println("GET  http://localhost:8080/api/sach");
+        System.out.println("GET  http://localhost:8080/api/bandoc");
+        System.out.println("POST http://localhost:8080/api/muon");
+        System.out.println("POST http://localhost:8080/api/tra");
+        System.out.println("POST http://localhost:8080/api/lichsu");
+
+    }
+
 }
